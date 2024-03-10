c3dc0cb16b7457c85945a4c27431c891ffa3c41d
Daniel Price <64694785+daniel-price@users.noreply.github.com>
2024-03-10 08:01:19 +0000

    Convert Home page to Page.new

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,68 @@
-module Pages.Home_ exposing (page)
+module Pages.Home_ exposing (Model, Msg, page)
 
+import Effect exposing (Effect)
 import Html
+import Page exposing (Page)
+import Route exposing (Route)
+import Shared
 import View exposing (View)
 
 
-page : View msg
-page =
+page : Shared.Model -> Route () -> Page Model Msg
+page _ _ =
+    Page.new
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Effect Msg )
+init () =
+    ( {}
+    , Effect.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp Never
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        NoOp _ ->
+            ( model, Effect.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view _ =
     { title = "Homepage"
     , body = [ Html.text "Hello, world!" ]
     }
